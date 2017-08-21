f9281a2da254b6680a53c786356cfacf3b80829c
xulon <yousuchen@126.com>
2017-08-21 16:33:58 +0800

    Create memo.h

---
+++ memo.h
@@ -0,0 +1,26 @@
+#ifndef MEMOPOOL_H
+#define MEMOPOOL_H
+
+#include<QVector>
+class memoPool
+{
+public:
+    memoPool();
+    int init(int count,int size);
+    void uninit();
+    void clear();
+    int pushdata(char*data,int len);
+    int popdata(char*data,int len);
+
+
+private:
+    QVector<char*> m_memory;
+    int m_count;
+    int m_size;
+    int __in;
+    int __out;
+    int __inposition;
+    int __outposition;
+};
+
+#endif // MEMOPOOL_H
